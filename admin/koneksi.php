3baf45905ea61fbaf41fab8cdb656d11ebd13045
godlovesmei <meiskesahertian7@gmail.com>
2025-01-08 13:35:12 +0700

    koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -2,9 +2,12 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "pure_beauty_db"; //Nama Database 
-// melakukan koneksi ke db 
+$db = "pure_beauty_db"; // Nama Database
+
+// Melakukan koneksi ke database
 $koneksi = mysqli_connect($host, $user, $pass, $db);
+
+// Memeriksa apakah koneksi berhasil
 if (!$koneksi) {
-    echo "Gagal konek: " . die(mysqli_error($koneksi));
+    die("Gagal konek: " . mysqli_connect_error());
 }
